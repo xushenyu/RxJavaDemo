25772a7201adc9366c206d16e15082ceb40b3b32
xushenyu <1003444938@qq.com>
2017-08-10 12:09:56 +0800

    转换练习

--- app/src/main/java/com/xsy/rxjavademo/MainActivity.java
+++ app/src/main/java/com/xsy/rxjavademo/MainActivity.java
@@ -1,6 +1,8 @@
 package com.xsy.rxjavademo;
 
+import android.graphics.Color;
 import android.os.Bundle;
+import android.support.v4.widget.SwipeRefreshLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.View;
@@ -14,6 +16,7 @@ import com.xsy.rxjavademo.retrofit.RetrofitAPI;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.concurrent.TimeUnit;
 
 import okhttp3.OkHttpClient;
 import retrofit2.Call;
@@ -27,6 +30,7 @@ import rx.Subscriber;
 import rx.Subscription;
 import rx.android.schedulers.AndroidSchedulers;
 import rx.functions.Action1;
+import rx.functions.Func1;
 import rx.schedulers.Schedulers;
 
 /**
@@ -34,21 +38,33 @@ import rx.schedulers.Schedulers;
  */
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
-    private static OkHttpClient okHttpClient = new OkHttpClient();
+    private static OkHttpClient okHttpClient;
     private Subscription subscription;
     private GetAPI getAPI;
     private PostAPI postAPI;
     private RetrofitAPI retrofitAPI;
+    private SwipeRefreshLayout mRefresh;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        mRefresh = (SwipeRefreshLayout) findViewById(R.id.refresh);
+        mRefresh.setColorSchemeColors(Color.BLACK, Color.GREEN, Color.RED);
+        mRefresh.setEnabled(false);
         findViewById(R.id.bt_one).setOnClickListener(this);
         findViewById(R.id.bt_two).setOnClickListener(this);
         findViewById(R.id.bt_three).setOnClickListener(this);
         findViewById(R.id.bt_four).setOnClickListener(this);
-
+        findViewById(R.id.bt_five).setOnClickListener(this);
+        okHttpClient = new OkHttpClient.Builder()
+                .connectTimeout(60, TimeUnit.SECONDS)//连接超时时间
+                .writeTimeout(60, TimeUnit.SECONDS)//写操作 超时时间
+                .readTimeout(60, TimeUnit.SECONDS)//读操作超时时间
+                .build();
+        /**
+         * 初始化Retrofit,使用okHttp请求，支持Gson解析，支持RxJava
+         */
         Retrofit retrofit = new Retrofit.Builder().client(okHttpClient)
                 .baseUrl("http://www.zhuangbi.info/")
                 .addConverterFactory(GsonConverterFactory.create())
@@ -61,20 +77,35 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
     @Override
     public void onClick(View v) {
+        mRefresh.setRefreshing(true);
         switch (v.getId()) {
             case R.id.bt_one://RxJava基本练习
-                Observable.just("这是just发送的数据1", "这是just发送的数据2").subscribe(new Action1<String>() {
+                subscription = Observable.just("这是just发送的数据1", "这是just发送的数据2").subscribe(new Action1<String>() {
                     @Override
                     public void call(String s) {
+                        mRefresh.postDelayed(new Runnable() {
+                            @Override
+                            public void run() {
+                                mRefresh.setRefreshing(false);
+                            }
+                        },500);
                         Toast.makeText(MainActivity.this, s, Toast.LENGTH_SHORT).show();
                     }
                 });
                 break;
             case R.id.bt_two://RxJava+Retrofit+OkHttp练习
                 subscription = getAPI.get("110")
+                        /**
+                         * subscribeOn(): 指定 subscribe() 所发生的线程，
+                         * 即 Observable.OnSubscribe 被激活时所处的线程。或者叫做事件产生的线程
+                         */
                         .subscribeOn(Schedulers.io())//让被观察者在IO线程运行，被观察者做的操作是网络请求
+                        /**
+                         * observeOn(): 指定 Subscriber 所运行在的线程。
+                         * 或者叫做事件消费的线程。
+                         */
                         .observeOn(AndroidSchedulers.mainThread())//让观察者在主线程运行，观察者操作是请求成功后的处理
-                        .subscribe(new Subscriber<List<ZhuangbiImage>>() {
+                        .subscribe(new Subscriber<List<ZhuangbiImage>>() {//订阅
                             @Override
                             public void onCompleted() {
 
@@ -82,11 +113,18 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
                             @Override
                             public void onError(Throwable e) {
+                                mRefresh.setRefreshing(false);
                                 Log.e("flag--", "onError(MainActivity.java:97)-->>" + e.getMessage());
                             }
 
                             @Override
                             public void onNext(List<ZhuangbiImage> s) {
+                                mRefresh.postDelayed(new Runnable() {
+                                    @Override
+                                    public void run() {
+                                        mRefresh.setRefreshing(false);
+                                    }
+                                },500);
                                 Toast.makeText(MainActivity.this, "get请求成功--" + s.get(0).description, Toast.LENGTH_SHORT).show();
                             }
                         });
@@ -105,11 +143,17 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
                             @Override
                             public void onError(Throwable e) {
-
+                                mRefresh.setRefreshing(false);
                             }
 
                             @Override
                             public void onNext(List<ZhuangbiImage> zhuangbiImages) {
+                                mRefresh.postDelayed(new Runnable() {
+                                    @Override
+                                    public void run() {
+                                        mRefresh.setRefreshing(false);
+                                    }
+                                },500);
                                 Toast.makeText(MainActivity.this, "post请求成功--" + zhuangbiImages.get(0).description, Toast.LENGTH_SHORT).show();
                             }
                         });
@@ -119,18 +163,49 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
                 call.enqueue(new Callback<List<ZhuangbiImage>>() {
                     @Override
                     public void onResponse(Call<List<ZhuangbiImage>> call, Response<List<ZhuangbiImage>> response) {
+                        mRefresh.postDelayed(new Runnable() {
+                            @Override
+                            public void run() {
+                                mRefresh.setRefreshing(false);
+                            }
+                        },500);
                         Toast.makeText(MainActivity.this, "普通Retrofit请求--" + response.body().get(0).description, Toast.LENGTH_SHORT).show();
                     }
 
                     @Override
                     public void onFailure(Call<List<ZhuangbiImage>> call, Throwable t) {
-                        Log.e("flag--","onFailure(MainActivity.java:126)-->>"+t.getMessage());
+                        mRefresh.setRefreshing(false);
+                        Log.e("flag--", "onFailure(MainActivity.java:126)-->>" + t.getMessage());
+                    }
+                });
+                break;
+            case R.id.bt_five://map转换 List<ZhuangbiImage>转换成String
+                 subscription = getAPI.get("可爱").map(new Func1<List<ZhuangbiImage>, String>() {
+                    @Override
+                    public String call(List<ZhuangbiImage> zhuangbiImages) {
+                        String description = zhuangbiImages.get(0).description;
+                        return description;
+                    }
+                }).subscribeOn(Schedulers.io()).observeOn(AndroidSchedulers.mainThread()).subscribe(new Action1<String>() {
+                    @Override
+                    public void call(String s) {
+                        mRefresh.postDelayed(new Runnable() {
+                            @Override
+                            public void run() {
+                                mRefresh.setRefreshing(false);
+                            }
+                        },500);
+                        Toast.makeText(MainActivity.this, "RxJava的map转换--"+s, Toast.LENGTH_SHORT).show();
                     }
                 });
                 break;
         }
     }
 
+    /**
+     * 在 subscribe() 之后， Observable 会持有 Subscriber 的引用，
+     * 这个引用如果不能及时被释放，将有内存泄露的风险
+     */
     @Override
     protected void onDestroy() {
         super.onDestroy();
